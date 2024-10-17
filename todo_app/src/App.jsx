3308b8f49d0e639b188fdf68fbd9f7a044e4eab1
LiamPTalbot <liam.p.talbot@gmail.com>
2024-10-17 21:10:32 +0100

    not getting anything out of this. Sacking it off at 1:38:16

--- todo_app/src/App.jsx
+++ todo_app/src/App.jsx
@@ -2,21 +2,39 @@ import { Header } from "./compnents/Header"
 import { Tabs } from "./compnents/Tabs"
 import { TodoInput } from "./compnents/TodoInput"
 import { TodoList } from "./compnents/TodoList"
+import { useState } from 'react'
 
 function App() {
-  const todos = [
-    { input: 'Hello! Add your first todo!', complete: true },
-    { input: 'Get the grocieries!', complete: false },
-    { input: 'Learm how to web design', complete: true },
-    { input: 'Say hi to new job', complete: true },
-  ]
+  // const todos = [
+  //   { input: 'Hello! Add your first todo!', complete: true },
+  //   { input: 'Get the grocieries!', complete: false },
+  //   { input: 'Learm how to web design', complete: true },
+  //   { input: 'Say hi to new job', complete: true },
+  // ]
+
+  const [todos, setTodos] = useState([
+    { input: 'Hello! Add your first todo!', complete: true }
+  ])
   
+  function handleAddTodo(newTodo) {
+    const newTodoList = [...todos, { input: newTodo, complete: false }]
+    setTodos(newTodoList)
+  }
+
+  function handleEditTodo() {
+
+  }
+
+  function handleDeleteTodo() {
+    
+  }
+
   return (
     <>
     <Header todos={todos} />
     <Tabs todos={todos} />
     <TodoList todos={todos} />
-    <TodoInput />
+    <TodoInput handleAddTodo={handleAddTodo} />
     </>
   )
 }
